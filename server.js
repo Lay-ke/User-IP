0ae769cb04185b7b4a55fffbff3cb03c7d2ded96
Mintah <yawmintah1@gmail.com>
2024-07-01 22:22:38 +0000

    changes made to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const ipstackAccessKey = process.env.IPSTACKACCESSKEY;
 const PORT = process.env.PORT;
 
-app.get('/home', async (req, res) => {
+app.get('/api/home', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
     console.log('User IP:', ip);
     const {visitor_name} = req.query;
