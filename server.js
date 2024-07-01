eef57851247bba6d8f7ae5cd39262a0fd8770a14
Mintah <yawmintah1@gmail.com>
2024-07-01 17:55:23 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.listen( 5001, ()=> {
+    console.log('Listening on port 5001...')
+});
+
+app.get('/', (req, res) => {
+    console.log(req.socket.remoteAddress)
+    res.json({Message: 'yeah'})
+    console.log('we inside!')
+})
