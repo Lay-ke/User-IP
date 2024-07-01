19a216aa7c94954198ba8aad5e04a0d0c4d2bfba
Mintah <yawmintah1@gmail.com>
2024-07-01 20:58:30 +0000

    added city location in server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+const ipstackAccessKey = '70e69423d81ff520fd6d39d335eecde1';
+
+app.get('/', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
     console.log('User IP:', ip);
-    res.json({Message: 'yeah', Your_IP: ip})
+    try {
+        const response = await axios.get(`http://api.ipstack.com/${ip}?access_key=${ipstackAccessKey}`);
+        const city = response.data.city;
+        console.log('User City:', city);
+        res.send({ Message: 'yeah', IP: ip, City: city });
+    } catch (error) {
+        console.error('Error fetching city information:', error);
+        res.status(500).send({ Message: 'Error fetching city information' });
+    }
 })
 
 app.listen( 5000, ()=> {
