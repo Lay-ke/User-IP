503c65223816271098a115399c7f16868a8b27ba
Mintah <yawmintah1@gmail.com>
2024-07-01 19:55:59 +0000

    changes in server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     console.log(req.socket.remoteAddress)
-    res.json({Message: 'yeah', Your_IP: req.socket.remoteAddress})
+    res.json({Message: 'yeah', Your_IP: req.ip})
     console.log('we inside!') 
 })
 
