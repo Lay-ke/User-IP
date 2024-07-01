8e6f7055a0e2221e85749e94de7493200ea12463
Mintah <yawmintah1@gmail.com>
2024-07-01 19:03:27 +0000

    changes to server js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen( 5001, ()=> {
-    console.log('Listening on port 5001...')
-});
-
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
     console.log(req.socket.remoteAddress)
     res.send({Message: 'yeah'})
     console.log('we inside!') 
 })
+
+app.listen( 5000, ()=> {
+    console.log('Listening on port 5000...')
+});
+
