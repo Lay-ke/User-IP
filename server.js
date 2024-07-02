afdfc9a91f9dd8a68ce70b38cc2164fbf696872b
Mintah <yawmintah1@gmail.com>
2024-07-02 13:12:16 +0000

    added temperature api

--- server.js
+++ server.js
@@ -6,18 +6,30 @@ const app = express();
 app.use(express.json());
 
 const ipstackAccessKey = process.env.IPSTACKACCESSKEY;
+const openWeatherMapApiKey = process.env.OPENWEATHERKEY;
+
 const PORT = process.env.PORT;
 
-app.get('/home', async (req, res) => {
+app.get('/api/home', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
     console.log('User IP:', ip);
     const {visitor_name} = req.query;
-    console.log(visitor_name);
+
     try {
-        const response = await axios.get(`http://api.ipstack.com/${ip}?access_key=${ipstackAccessKey}`);
-        const city = response.data.city;
-        console.log('User City:', city);
-        res.send({client_ip: ip, location: city, greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${city}`});
+         // Get location details from ipstack
+         const ipstackResponse = await axios.get(`http://api.ipstack.com/${ip}?access_key=${ipstackAccessKey}`);
+         const { city, latitude, longitude } = ipstackResponse.data;
+         console.log('User City:', city);
+ 
+         if (!city) {
+             throw new Error('City not found for IP address');
+         }
+ 
+         // Get weather details from OpenWeatherMap
+         const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${openWeatherMapApiKey}`);
+
+         const temperature = weatherResponse.data.main.temp;
+        res.send({client_ip: ip, location: city, greeting: `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celcius in ${city}`});
     } catch (error) {
         console.error('Error fetching city information:', error);
         res.status(500).send({ Message: 'Error fetching city information' });
